1938c54ac00ddc9e334d10c465edf0633041bfa1
shlopezpe <shlopezpe@unal.edu.co>
2025-11-20 21:26:00 -0500

    adds santander polygons

---
+++ 01_generate_polygon_santander.py
@@ -0,0 +1,34 @@
+import os
+import requests
+import geopandas as gpd
+
+def gen_pol():
+    url = "https://raw.githubusercontent.com/caticoa3/colombia_mapa/master/co_2018_MGN_MPIO_POLITICO.geojson"
+
+    # Descargar archivo
+    print("Descargando GeoJSON desde GitHub...")
+    response = requests.get(url)
+    response.raise_for_status()
+
+    geojson_data = response.json()
+
+    print("Convirtiendo a GeoDataFrame...")
+    gdf = gpd.GeoDataFrame.from_features(geojson_data["features"])
+
+    print("Filtrando municipios del departamento de Santander...")
+    gdf_santander = gdf[gdf["DPTO_CCDGO"] == "68"].copy()
+
+    print(f"Total municipios encontrados: {gdf_santander.shape[0]}")
+
+    output_dir = "data/bronze/dane_geo"
+    os.makedirs(output_dir, exist_ok=True)
+
+    geojson_path = os.path.join(output_dir, "santander_municipios.geojson")
+
+    print(f"Guardando GeoJSON en: {geojson_path}")
+    gdf_santander.to_file(geojson_path, driver="GeoJSON")
+
+    print("Proceso completado con éxito.")
+
+if __name__ == "__main__":
+    gen_pol()
